3d4ccc16e3cb2dcda454adb38df246ce335879cd
Nicolai Müller <nicolaimu1212@gmail.com>
2025-07-04 16:11:18 +0200

    Code along regression

---
+++ housing/data_utils.py
@@ -0,0 +1,45 @@
+import os
+import tarfile
+import urllib
+import pandas as pd
+import numpy as np
+from zlib import crc32
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+def test_set_check(identifier, test_ratio):
+    """
+    Computes a has value for each instance's identifier and puts an instance in the test set, if the hash is lower
+    than 20 % of the maximum hash value.
+    """
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    # not in test set, in test set
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+def split_train_test(data, test_ratio):
+    """
+    Naive train/test split function with no reproducibility
+    """
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
