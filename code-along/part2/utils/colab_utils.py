7a2427305c982857372fd5f0df23279ad733b758
Nicolai Müller <nicolaimu1212@gmail.com>
2025-07-29 09:17:24 +0200

    Simplify code

--- code-along/part2/utils/colab_utils.py
+++ code-along/part2/utils/colab_utils.py
@@ -23,7 +23,8 @@ def delete_directory(dir_name):
 def move_and_unzip_file(file_name):
     local_zip_file = os.path.join(local_path, file_name)
 
-    move_file(file_name)
+    if not os.path.exists(local_zip_file):
+        move_file(file_name)
 
     log.info(f'Unzipping file {file_name}')
 
@@ -37,22 +38,28 @@ def move_and_unzip_file(file_name):
 
     os.remove(local_zip_file)
 
+def _execute_subset_load(subset_list, num_workers=None):
+    subset_list = [subset for subset in subset_list if not os.path.exists(os.path.join(local_path, subset))]
+    num_workers = len(subset_list) if num_workers is None else num_workers
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+        futures = [
+            executor.submit(move_and_unzip_file, f'{subset}.zip') for subset in subset_list
+        ]
+        concurrent.futures.wait(futures)
+
+
 def fetch_data(subset=None):
     if not os.path.exists(local_path):
         os.makedirs(local_path)
 
     if subset is None:
-        missing_subsets = []
-        for subset_index in range(10):
-            subset = f'subset{subset_index}'
-            if not os.path.exists(os.path.join(local_path, subset)):
-                missing_subsets.append(subset)
-
-        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-            futures = [
-                executor.submit(move_and_unzip_file, f'{subset}.zip') for subset in missing_subsets
-            ]
-            concurrent.futures.wait(futures)
+        # Get all subsets
+        missing_subsets = [f'subset{subset_index}' for subset_index in range(10)]
+        _execute_subset_load(missing_subsets)
+    elif len(subset) > 1:
+        # Get all subsets in list in parallel
+        subset_list = [f'subset{subset_index}' for subset_index in subset]
+        _execute_subset_load(subset_list)
     else:
         move_and_unzip_file(f'subset{subset}.zip')
 
