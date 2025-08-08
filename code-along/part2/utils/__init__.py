b64c598ec49ad2d44fd4c53f0ce7dce988a637f6
Nicolai Müller <nicolaimu1212@gmail.com>
2025-08-08 09:30:54 +0200

    add unet model
    In this commit, a standard implementation of the UNet model is added.

--- code-along/part2/utils/__init__.py
+++ code-along/part2/utils/__init__.py
@@ -3,6 +3,7 @@ from .disk import get_cache
 from .config import set_mode, get_mode, get_data_root
 from .util import XyzTuple, xyz2irc, importstr, enumerate_with_estimate
 from .colab_utils import move_file, move_and_unzip_file, fetch_data, delete_directory
+from .unet import UNet
 
 __all__ = [
     "logging",
@@ -17,5 +18,6 @@ __all__ = [
     "move_file",
     "move_and_unzip_file",
     "fetch_data",
-    "delete_directory"
+    "delete_directory",
+    "UNet"
 ]
